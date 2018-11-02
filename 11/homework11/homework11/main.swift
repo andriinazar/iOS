c1ca40372c6f0c59834b2859593f01d608f4e190
andriinazar <nazar.andrii@gmail.com>
2018-11-02 21:04:47 +0200

    create project

---
+++ 11/homework11/homework11/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  homework11
+//
+//  Created by Andrii Nazar on 11/2/18.
+//  Copyright © 2018 Andrii Nazar. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ 11/homework11/homework11/projects/iOS/courses/11/homework11/homework11/projects/iOS/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+var test = 12
+var test2 = test * 15
+for item
+    in UIActivityItemSource
+{
+    <#code#>
+}
